e8c5f4105a507fbc3b3a915863f1de9bdea1e0fe
Ruloff Tom <23336472+TruLov@users.noreply.github.com>
2024-03-09 18:54:24 +0100

    bootstrap debugging

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -7,8 +7,9 @@ return {
             sources = {
                 null_ls.builtins.formatting.stylua,
                 null_ls.builtins.formatting.prettier,
-                null_ls.builtins.formatting.erb_lint,
-                null_ls.builtins.formatting.eslint_d,
+                --null_ls.builtins.formatting.erb_lint,
+                --null_ls.builtins.formatting.eslint_d,
+                --null_ls.builtins.formatting.cds_lsp,
             },
         })
 
