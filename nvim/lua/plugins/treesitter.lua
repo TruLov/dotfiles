b6539286631d4de04424afa3ada13ca7ccf9c28a
Ruloff Tom <23336472+TruLov@users.noreply.github.com>
2024-03-09 18:53:58 +0100

    Bump deps

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,22 +1,21 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function()
-		local config = require("nvim-treesitter.configs")
-		config.setup({
-			auto_install = true,
-			highlight = { enable = true },
-			indent = { enable = true },
-		})
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+        local config = require("nvim-treesitter.configs")
+        config.setup({
+            auto_install = true,
+            highlight = { enable = true },
+            indent = { enable = true },
+        })
 
-		local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-		parser_config.cds = {
-			install_info = {
-				-- local path or git repo
-				url = "~/projects/tree-sitter-cds",
-				files = { "src/parser.c", "src/scanner.c" },
-			},
-			filetype = "cds",
-		}
-	end,
+        local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+        parser_config.cds = {
+            install_info = {
+                url = "~/projects/tree-sitter-cds",
+                files = { "src/parser.c", "src/scanner.c" },
+            },
+            filetype = "cds",
+        }
+    end,
 }
